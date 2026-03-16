6da498a2a44bc7f12637ab5251495050183dc4ad
Silletr <hristicenkoigor764@gmail.com>
2026-03-16 17:06:16 +0000

    [NEW FILE/DIR: poetry.toml | CHANGED FILE/DIR: build.sh, install.sh, poetry.lock, pyproject.toml] Yes, current week is for LazyDeveloperHelper, but you can count it as emergence fix for work in all platforms without errors, and for work from box [TIMESTAMP: 16/03/2026 | 19:06 UTC+2]

--- build.sh
+++ build.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-
+set -e
 cd src/movie_streaming_silletr
-
 pyinstaller --onefile \
   --name="SiMovieStreaming" \
   --add-data="core/genres.json:core" \
   --add-data="styles.tcss:." \
+  --collect-all=python_dotenv \
+  --collect-all=dotenv \
+  --hidden-import=python_dotenv \
+  --hidden-import=dotenv \
+  --hidden-import=load_dotenv \
   --paths="." \
   gui/main_ui.py
-
+mkdir -p ../../dist
 mv dist/SiMovieStreaming ../../dist/SiMovieStreaming-linux
 rm -rf dist/ build/ *.spec
 cd ../..
--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo cp dist/tui /usr/local/bin/simoviestream
+sudo cp dist/SiMovieStreaming-linux /usr/local/bin/simoviestream
 chmod +x /usr/local/bin/simoviestream
 echo "installed ✅"
